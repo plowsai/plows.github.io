1f68245fed6dfb0c004e800dd102ca7172bc41eb
Jordan Plows <plowstjordan@gmail.com>
2022-11-13 14:19:18 -0800

    dev: first major commit

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@
 [1, 2, 3].forEach(function (x) {
   return showItWorks();
 });
-
 function showItWorks() {
   document.getElementById("javascript-status").innerHTML = "JavaScript transpilation works!";
 }
